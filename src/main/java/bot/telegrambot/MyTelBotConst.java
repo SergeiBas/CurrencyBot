ea017ab2454808691d5258b64696c8dcae7a3db1
SergeiBas <sergei.bas06@gmail.com>
2025-06-04 13:28:04 +0300

    commit and push

--- src/main/java/bot/telegrambot/MyTelBotConst.java
+++ src/main/java/bot/telegrambot/MyTelBotConst.java
@@ -1,6 +1,6 @@
 package bot.telegrambot;
 
 public class MyTelBotConst {
-    public static final String MY_TEL_BOT_NAME = "CNUcurrencybot";
-    public static final String MY_TEL_BOT_TOKEN = "7676219466:AAFrGaes1DIRhxWrCLPoSKRlV2WAvpvuWCo";
+    public static final String MY_TEL_BOT_NAME = "";
+    public static final String MY_TEL_BOT_TOKEN = "";
 }
